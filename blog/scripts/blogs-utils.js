8c019fd5dc5194690cea58bf6728ed7a8d56d369
Pasika Sriwanna <pasika.swn@gmail.com>
2024-02-08 21:37:14 +0700

    checkpoint 1

--- blog/scripts/blogs-utils.js
+++ blog/scripts/blogs-utils.js
@@ -1,47 +1,34 @@
-const blogElement = document.getElementById("blog-container");
+const blogElement = document.getElementById("blog-container")
+function createBlogHTML(blogs){
+  const blogContentElement = blogs.map(function (blog) {
+    return `<div class="flex flex-col md:flex-row gap-6 w-full">
+    <img
+        src="${blog.imageUrl}"
+        alt="feature image 1"
+        class="w-full md:w-auto"
+    />
+    <div class="flex flex-col gap-4 bg-wd-darkgrey p-6 grow">
+        <h3 class="text-2xl font-semibold">
+        ${blog.title}
+        </h3>
+        <p class="text-xl font-light">
+        ${blog.description}
+        </p>
+        <p>At ${blog.publishedDate}</p>
+        <a href="blogs/test.html">Read more</a>
+    </div>
+    </div>`
+  }).join("")
 
-// create fn
-function createBlogHTML(blogs) {
-  // วนแต่ละตัวของ blogs ด้วย .map เพื่อทำการแปลงเ็น html ออกมา
-  const blogContentElement = blogs
-  .map(function (blog) {
-    return `
-    <div class="flex flex-col md:flex-row gap-6 w-full">
-        <img
-            src="${blog.imageUrl}"
-            alt="feature image 1"
-            class="w-full md:w-auto"
-        />
-        <div class="flex flex-col gap-4 bg-wd-darkgrey p-6 grow">
-            <h3 class="text-2xl font-semibold">
-            ${blog.title}
-            </h3>
-            <p class="text-xl font-light">
-            ${blog.description}
-            </p>
-            <p>At ${blog.publishedDate}</p>
-            <a href="blogs/test.html">Read more</a>
-        </div>
-        </div>
-    
+  blogElement.innerHTML = blogContentElement
 
-    `;
-    })
-    .join("");
-    
-    // ต่อ html ทั้งหมดเพื่อใส่ใน blogElement.innerHTML
-  blogElement.innerHTML = blogContentElement;
 }
-
 
 async function main() {
-  const response = await axios.get("/scripts/blogs.json");
-  const blogs = response.data;
-  
-  //ทำการนำ response.data ส่งเข้าไปใน createBlogHTML เป็น array ของ blogs
-  createBlogHTML(blogs);
+  const response = await axios.get("/scripts/blogs.json")  
+  const blogs = response.data
 
-
+  createBlogHTML(blogs)
 }
 
-main();
+main()
--- blog/scripts/leaninng_dotjoin.js
+++
@@ -1,5 +0,0 @@
-let data = ['mike', 'skooldio', 'bootcamp']
-
-data.map(function(d) {
-    return `<b>${d}</b>`
-}).join('-')
