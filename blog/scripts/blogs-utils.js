a7b5548cae8eceff3e6d9f6f79ebaebce8160d65
Pasika Sriwanna <pasika.swn@gmail.com>
2024-02-15 11:40:26 +0700

    none

--- blog/scripts/blogs-utils.js
+++ blog/scripts/blogs-utils.js
@@ -1,13 +1,14 @@
 //access by id "blog-container"
-const blogElement = document.getElementById("blog-container")
-let blogsRawData = []
-let loadingTimeout = {}
+const blogElement = document.getElementById("blog-container");
+let blogsRawData = [];
+let loadingTimeout = {};
 
 //write a fn to change innerHTML
 function createBlogHTML(blogs) {
-    ///เอา json แต่ละก้อนมา .map เพื่อให้ได้ html ย่อยๆที่เปลี่ยนไปตาม json obj -> จากนั้นค่อยใช้ .join เพื่อเชื่อมอันย่อยๆให้กลายเป็นเนื้อเดียว
-    const blogContentElement = blogs.map(function (blog) {
-        return `<div class="flex flex-col md:flex-row gap-6 w-full">
+  ///เอา json แต่ละก้อนมา .map เพื่อให้ได้ html ย่อยๆที่เปลี่ยนไปตาม json obj -> จากนั้นค่อยใช้ .join เพื่อเชื่อมอันย่อยๆให้กลายเป็นเนื้อเดียว
+  const blogContentElement = blogs
+    .map(function (blog) {
+      return `<div class="flex flex-col md:flex-row gap-6 w-full">
         <img
             src="${blog.imageUrl}"
             alt="feature image 1"
@@ -23,15 +24,15 @@ function createBlogHTML(blogs) {
             <p>At ${blog.publishedDate}</p>
             <a href="blogs/test.html">Read more</a>
         </div>
-        </div>`
-    }).join("")
+        </div>`;
+    })
+    .join("");
 
-    ///เอาทั้งก้อนที่ return ไปเปลี่ยนแปลง blogElement -> blogElement.innerHTML
-    blogElement.innerHTML = blogContentElement
+  ///เอาทั้งก้อนที่ return ไปเปลี่ยนแปลง blogElement -> blogElement.innerHTML
+  blogElement.innerHTML = blogContentElement;
 }
 
-
-// //write searchFN 
+// //write searchFN
 // function searchBlogs(element) {
 //     //เอา json มาหาfilter
 //     const filteredBlogs = blogsRawData.filter(function (blog) {
@@ -42,32 +43,30 @@ function createBlogHTML(blogs) {
 //     createBlogHTML(filteredBlogs)
 // }
 
-//write searchFN 
+//write searchFN
 function searchBlogs(element) {
-    //write "loading..." by using setTimeout()
-    clearTimeout(loadingTimeout)
-    blogElement.innerHTML = "loading..." //when typing search-text
+  clearTimeout(loadingTimeout);
+  blogElement.innerHTML = "loading~~~";
 
-    loadingTimeout = setTimeout(function () {
-        const filteredBlogs = blogsRawData.filter(function (blog) {
-            return blog.title.includes(element.value) || blog.description.includes(element.value)
-        })
-    
-        createBlogHTML(filteredBlogs)
-        
-    },2000)
+  loadingTimeout = setTimeout(function () {
+    const filteredBlogs = blogsRawData.filter(function (blog) {
+      return (
+        blog.title.includes(element.value) ||
+        blog.description.includes(element.value)
+      );
+    });
 
+    createBlogHTML(filteredBlogs);
+  }, 2000);
 }
-
-
 
 //fetch API from blogs.json
 async function main() {
-    const response = await axios.get("/scripts/blogs.json")
-    blogsRawData = response.data
+  const response = await axios.get("/scripts/blogs.json");
+  blogsRawData = response.data;
 
-    createBlogHTML(blogsRawData)
-    console.log("enter to main")
+  createBlogHTML(blogsRawData);
+  console.log("enter to main");
 }
 
-main()
+main();
